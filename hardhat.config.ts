c1bde571d770827200794d2fe83a552a755cfb24
yushaku <levanson180200@gmail.com>
2023-08-18 18:04:13 +0700

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+
+dotenv.config();
+const {
+  WALLET_PRIVATE_KEY = "",
+  ETHERSCAN_API_KEY = "",
+  ALCHEMY_API_URL = "",
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    ropsten: {
+      url: "",
+      accounts: [WALLET_PRIVATE_KEY],
+    },
+    sepolia: {
+      url: ALCHEMY_API_URL,
+      chainId: 11155111,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
