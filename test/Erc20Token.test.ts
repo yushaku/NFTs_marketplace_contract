a9a270a74252a60e854625aabe8186f3b3f064c4
yushaku <levanson180200@gmail.com>
2023-11-20 13:44:58 +0700

    add test

--- test/Erc20Token.test.ts
+++ test/Erc20Token.test.ts
@@ -5,7 +5,7 @@ import { ethers } from "hardhat";
 describe("Token contract", function () {
   async function deployTokenFixture() {
     const [owner, addr1, addr2] = await ethers.getSigners();
-    const hardhatToken = await ethers.deployContract("PoliteCatToken");
+    const hardhatToken = await ethers.deployContract("Yushaku");
     return { hardhatToken, owner, addr1, addr2 };
   }
 
@@ -19,25 +19,23 @@ describe("Token contract", function () {
 
   describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
-      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
-        deployTokenFixture
-      );
+      const { hardhatToken, owner, addr1, addr2 } =
+        await loadFixture(deployTokenFixture);
       // Transfer 50 tokens from owner to addr1
       await expect(
-        hardhatToken.transfer(addr1.address, 50)
+        hardhatToken.transfer(addr1.address, 50),
       ).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50]);
 
       // Transfer 50 tokens from addr1 to addr2
       // We use .connect(signer) to send a transaction from another account
       await expect(
-        hardhatToken.connect(addr1).transfer(addr2.address, 50)
+        hardhatToken.connect(addr1).transfer(addr2.address, 50),
       ).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50]);
     });
 
     it("Should emit Transfer events", async function () {
-      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
-        deployTokenFixture
-      );
+      const { hardhatToken, owner, addr1, addr2 } =
+        await loadFixture(deployTokenFixture);
 
       // Transfer 50 tokens from owner to addr1
       await expect(hardhatToken.transfer(addr1.address, 50))
@@ -52,20 +50,19 @@ describe("Token contract", function () {
     });
 
     it("Should fail if sender doesn't have enough tokens", async function () {
-      const { hardhatToken, owner, addr1 } = await loadFixture(
-        deployTokenFixture
-      );
+      const { hardhatToken, owner, addr1 } =
+        await loadFixture(deployTokenFixture);
       const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
 
       // Try to send 1 token from addr1 (0 tokens) to owner.
       // `require` will evaluate false and revert the transaction.
       await expect(
-        hardhatToken.connect(addr1).transfer(owner.address, 1)
+        hardhatToken.connect(addr1).transfer(owner.address, 1),
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
 
       // Owner balance shouldn't have changed.
       expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance
+        initialOwnerBalance,
       );
     });
   });
