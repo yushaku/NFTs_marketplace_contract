yushaku/dex-contract

update shop payment

contracts/market/ShopPayment.sol
@@ -6,11 +6,13 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 /// @author yushaku
 /// @title ShopPayment
 /// @title ShopPayment - Upgradeable Smart Contract for E-commerce Payments
 /// @notice This contract allows users to create and pay for orders in a single step,
-/// cancel orders before 3 days with a 1% fee, mark orders as delivered, and enables the owner to withdraw funds from delivered orders.
 
 enum Status {
   PAID,
@@ -24,131 +26,221 @@ struct Order {
   uint256 price;
   Status status;
   uint256 createdAt;
+  address token;
 }
 
-// prettier-ignore
-contract ShopPayment is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable {
-  mapping(string => Order) internal orders;
-  mapping(address => string[]) internal userOrders;
+error OrderAlreadyCancelled(string orderId);
+error OrderDoesNotExist(string orderId);
+error OnlyPaidOrderCanBeCancel(string orderId);
+error SenderIsNotBuyer(address sender, address buyer);
+error InvalidPaymentToken(address token);
 
-  uint256 public totalWithdrawable;
+contract ShopPayment is
+  Initializable,
+  OwnableUpgradeable,
+  ReentrancyGuardUpgradeable,
+  UUPSUpgradeable
+{
+  using SafeERC20 for IERC20Metadata;
+
+  address public constant NATIVE_TOKEN = address(0);
+
+  mapping(string => Order) public orders;
+  mapping(address => string[]) public userOrders;
+
+  address[] internal allowedTokens;
+  mapping(address => bool) public payableToken;
+  mapping(address => uint256) public withdrawable;
 
   // [OPTIONAL] Add a storage gap to allow future versions to add new variables without shifting storage
   uint256[50] private __gap;
 
-  event OrderCreated(string orderId, address indexed buyer, uint256 price);
   event OrderPaid(string orderId, address indexed buyer, uint256 price);
-  event OrderCancelled(string orderId, uint256 refundAmount, uint256 feeAmount);
+  event OrderCancelled(string orderId, uint256 refundAmount);
   event OrderDelivered(string orderId);
-  event Withdrawn(uint256 amount);
+  event Withdrawn(address token, uint256 amount);
 
   /// @notice Initializer function (replaces constructor for upgradeable contracts)
   function initialize() public initializer {
     __Ownable_init(msg.sender); // Initialize Ownable
     __ReentrancyGuard_init(); // Initialize ReentrancyGuard
     __UUPSUpgradeable_init(); // Initialize UUPSUpgradeable
+
+    allowedTokens.push(NATIVE_TOKEN);
+    payableToken[NATIVE_TOKEN] = true;
+    withdrawable[NATIVE_TOKEN] = 0;
   }
 
   /// @notice Required by UUPSUpgradeable to authorize upgrades
-  function _authorizeUpgrade(
-    address newImplementation
-  ) internal override onlyOwner {}
+  function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+  modifier isPayableToken(address _payToken) {
+    require(_payToken == NATIVE_TOKEN || payableToken[_payToken], "invalid pay token");
+    _;
+  }
+
+  // MARK: USER FUNCTIONS
 
   /// @notice create a new order
   /// @param _orderId uuid of order
   /// @param _price price of order
   function createAndPayOrder(
     string calldata _orderId,
-    uint256 _price
-  ) external payable virtual nonReentrant {
+    uint256 _price,
+    address _payToken
+  ) external payable virtual nonReentrant isPayableToken(_payToken) {
     require(bytes(_orderId).length > 0, "Order ID cannot be empty");
     require(_price > 0, "Price must be greater than zero");
     require(orders[_orderId].buyer == address(0), "Order ID already exists");
-    require(msg.value == _price, "Incorrect payment amount");
+
+    _takeToken(_payToken, _price);
 
     Order memory newOrder = Order({
       orderId: _orderId,
       buyer: msg.sender,
       price: _price,
       status: Status.PAID,
-      createdAt: block.timestamp
+      createdAt: block.timestamp,
+      token: _payToken
     });
 
     orders[_orderId] = newOrder;
     userOrders[msg.sender].push(_orderId);
 
-    emit OrderCreated(_orderId, msg.sender, _price);
+    emit OrderPaid(_orderId, msg.sender, _price);
   }
 
   /// @notice cancel the order
   /// @param _orderId id of cancel order
-  function cancelOrder(string calldata _orderId) external nonReentrant {
-    Order storage order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
-    require(
-      msg.sender == order.buyer,
-      "Only the buyer or owner can cancel the order"
-    );
-    require(order.status == Status.PAID, "Only PAID orders can be cancelled");
-    require(
-      block.timestamp <= order.createdAt + 3 days,
-      "Can only cancel before 3 days"
-    );
+  function cancelOrder(string memory _orderId) external nonReentrant {
+    Order memory order = orders[_orderId];
+    if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+    if (msg.sender != order.buyer) revert SenderIsNotBuyer(msg.sender, order.buyer);
+    if (order.status != Status.PAID) revert OnlyPaidOrderCanBeCancel(_orderId);
 
-    uint256 refundAmount = (order.price * 99) / 100;
-    uint256 feeAmount = order.price - refundAmount;
-
-    // refund to the user
-    (bool success, ) = order.buyer.call{ value: refundAmount }("");
-    require(success, "Refund failed");
-    order.status = Status.CANCELLED;
-
-    emit OrderCancelled(_orderId, refundAmount, feeAmount);
+    _cancelOrder(_orderId);
   }
 
-  /// @notice Chuyển trạng thái đơn hàng sang Delivered
-  /// @param _orderId Mã đơn hàng cần chuyển trạng thái
-  function deliverOrder(string calldata _orderId) external onlyOwner {
-    Order storage order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
-    require(order.status == Status.PAID, "Only PAID orders can be delivered");
+  function batchCancelOrder(string[] calldata orderIds) external nonReentrant {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      string memory _orderId = orderIds[index];
+      Order memory order = orders[_orderId];
 
-    order.status = Status.DELIVERED;
-    totalWithdrawable += order.price;
+      //prettier-ignore
+      if (msg.sender != order.buyer) revert SenderIsNotBuyer(msg.sender, order.buyer);
+      if (order.status != Status.PAID) revert OnlyPaidOrderCanBeCancel(_orderId);
+      if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
 
-    emit OrderDelivered(_orderId);
+      _cancelOrder(_orderId);
+    }
   }
 
-  /// @notice Withdrawn all token
+  /// @notice admin force cancel order
+  /// @notice when order has paid and delivered still be canceled by admin
+  /// @param orderIds is a list of order
+  function forceCancelOrder(string[] calldata orderIds) external onlyOwner {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      string memory _orderId = orderIds[index];
+      Order memory order = orders[_orderId];
+
+      //prettier-ignore
+      if (order.status == Status.CANCELLED) revert OrderAlreadyCancelled(_orderId);
+      if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+
+      _cancelOrder(_orderId);
+    }
+  }
+
+  function deliverOrder(string[] memory orderIds) external onlyOwner {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      _deliverOrder(orderIds[index]);
+    }
+  }
+
   function withdrawAll() external onlyOwner nonReentrant {
-    require(totalWithdrawable > 0, "No funds available for withdrawal");
+    address owner = owner();
 
-    uint256 amount = totalWithdrawable;
-    totalWithdrawable = 0;
+    for (uint256 index = 0; index < allowedTokens.length; index++) {
+      address token = allowedTokens[index];
+      uint256 amount = withdrawable[token];
 
-    (bool success, ) = owner().call{ value: amount }("");
-    require(success, "Transfer failed.");
-
-    emit Withdrawn(amount);
+      if (amount > 0) {
+        _sendToken(token, owner, amount);
+        emit Withdrawn(token, amount);
+      }
+    }
   }
 
   /// @notice get order info
   /// @param _orderId id of the order
   /// @return Order info
-  function getOrder(
-    string calldata _orderId
-  ) external view returns (Order memory) {
+  function getOrder(string calldata _orderId) external view returns (Order memory) {
     Order memory order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
     return order;
   }
 
   /// @notice get list of order of user
   /// @param _buyer address of user
   /// @return string[] list of order id
-  function getUserOrders(
-    address _buyer
-  ) external view returns (string[] memory) {
+  function getUserOrders(address _buyer) external view returns (string[] memory) {
     return userOrders[_buyer];
+  }
+
+  // MARK: ADMIN FUNCTIONS
+  function addPayableToken(address _token) external onlyOwner {
+    if (_token == address(0)) revert InvalidPaymentToken(_token);
+    require(!payableToken[_token], "already payable token");
+
+    payableToken[_token] = true;
+    allowedTokens.push(_token);
+  }
+
+  function disableToken(address _token) external onlyOwner {
+    if (_token == address(0)) revert InvalidPaymentToken(_token);
+    payableToken[_token] = false;
+  }
+
+  // MARK: INTERNAL FUNCTIONS
+  function _cancelOrder(string memory _orderId) internal {
+    Order storage order = orders[_orderId];
+    order.status = Status.CANCELLED;
+
+    // REFUND TO THE USER
+    _sendToken(order.token, order.buyer, order.price);
+
+    emit OrderCancelled(_orderId, order.price);
+  }
+
+  function _deliverOrder(string memory _orderId) internal {
+    Order storage order = orders[_orderId];
+    if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+    require(order.status == Status.PAID, "Only PAID orders can be delivered");
+
+    order.status = Status.DELIVERED;
+    withdrawable[order.token] += order.price;
+
+    emit OrderDelivered(_orderId);
+  }
+
+  function _sendToken(address token, address to, uint256 amount) private {
+    if (amount == 0) return;
+
+    if (token == NATIVE_TOKEN) {
+      require(address(this).balance >= amount, "Insufficient balance in contract.");
+      (bool success, ) = to.call{ value: amount }("");
+      require(success, "Transfer failed.");
+    } else {
+      IERC20Metadata(token).safeTransfer(to, amount);
+    }
+  }
+
+  function _takeToken(address token, uint256 amount) internal {
+    if (amount == 0) return;
+
+    if (token == NATIVE_TOKEN) {
+      require(msg.value >= amount, "Incorrect payment amount");
+    } else {
+      IERC20Metadata(token).safeTransferFrom(msg.sender, address(this), amount);
+    }
   }
 }

scripts/2.deploy_shopPayment.ts
@@ -1,44 +1,46 @@
 import { ethers, upgrades } from "hardhat";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 import { getAddress, verifyContract, writeDownAddress } from "./utils/helper";
 import { ContractName } from "./utils/config";
 import { sleep } from "./utils/sleep";
 
 async function main(step: number) {
-  const [owner] = await ethers.getSigners();
+  // const [owner] = await ethers.getSigners();
   const network = await ethers.provider.getNetwork();
+  let implementationAddress = "0x96008434aee932f739925771a71325c354f0c861";
 
   if (step <= 1) {
     console.log("Deploying ShopPayment...");
     const ShopPayment = await ethers.getContractFactory("ShopPayment");
 
     // Deploy the upgradeable contract using a UUPS proxy
-    const shopPayment = await upgrades.deployProxy(
-      ShopPayment,
-      [owner.address],
-      {
-        initializer: "initialize",
-        kind: "uups", // Specifies the UUPS proxy pattern
-      },
-    );
+    const shopPayment = await upgrades.deployProxy(ShopPayment, [], {
+      initializer: "initialize",
+    });
 
     const address = await shopPayment.getAddress();
     console.info({ address });
+
+    implementationAddress = await getImplementationAddress(
+      ethers.provider,
+      address,
+    );
+    console.info("Implementation Address:", implementationAddress);
 
     writeDownAddress(ContractName.ShopPayment, address, network.name);
 
     await sleep(20_000);
   }
 
-  if (step <= 2) {
+  if (step <= 1.5) {
     const address = getAddress(ContractName.ShopPayment, network.name);
     await verifyContract(address, []);
 
-    const implementation = "0x4E3DdcCeEf165fC30F876cf81b4d7a80C2A1A7bD";
-    await verifyContract(implementation, []);
+    await verifyContract(implementationAddress, []);
   }
 }
 
-main(1)
+main(1.5)
   .then(() => process.exit(0))
   .catch((error: Error) => {
     console.error(error);

yushaku/NFTs_marketplace_contract

contracts/market/ShopPayment.sol
@@ -6,11 +6,13 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 /// @author yushaku
 /// @title ShopPayment
 /// @title ShopPayment - Upgradeable Smart Contract for E-commerce Payments
 /// @notice This contract allows users to create and pay for orders in a single step,
-/// cancel orders before 3 days with a 1% fee, mark orders as delivered, and enables the owner to withdraw funds from delivered orders.
 
 enum Status {
   PAID,
@@ -24,131 +26,225 @@ struct Order {
   uint256 price;
   Status status;
   uint256 createdAt;
+  address token;
 }
 
-// prettier-ignore
-contract ShopPayment is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable {
-  mapping(string => Order) internal orders;
-  mapping(address => string[]) internal userOrders;
+error OrderAlreadyCancelled(string orderId);
+error OrderDoesNotExist(string orderId);
+error OnlyPaidOrderCanBeCancel(string orderId);
+error SenderIsNotBuyer(address sender, address buyer);
+error InvalidPaymentToken(address token);
 
-  uint256 public totalWithdrawable;
+contract ShopPayment is
+  Initializable,
+  OwnableUpgradeable,
+  ReentrancyGuardUpgradeable,
+  UUPSUpgradeable
+{
+  using SafeERC20 for IERC20Metadata;
+
+  address public constant NATIVE_TOKEN = address(0);
+
+  mapping(string => Order) public orders;
+  mapping(address => string[]) public userOrders;
+
+  address[] internal allowedTokens;
+  mapping(address => bool) public payableToken;
+  mapping(address => uint256) public withdrawable;
 
   // [OPTIONAL] Add a storage gap to allow future versions to add new variables without shifting storage
   uint256[50] private __gap;
 
-  event OrderCreated(string orderId, address indexed buyer, uint256 price);
   event OrderPaid(string orderId, address indexed buyer, uint256 price);
-  event OrderCancelled(string orderId, uint256 refundAmount, uint256 feeAmount);
-  event OrderDelivered(string orderId);
-  event Withdrawn(uint256 amount);
+  event OrderCancelled(string orderId, address indexed buyer, uint256 refundAmount);
+  event OrderDelivered(string orderId, address indexed buyer);
+  event Withdrawn(address token, uint256 amount);
 
   /// @notice Initializer function (replaces constructor for upgradeable contracts)
   function initialize() public initializer {
     __Ownable_init(msg.sender); // Initialize Ownable
     __ReentrancyGuard_init(); // Initialize ReentrancyGuard
     __UUPSUpgradeable_init(); // Initialize UUPSUpgradeable
+
+    allowedTokens.push(NATIVE_TOKEN);
+    payableToken[NATIVE_TOKEN] = true;
+    withdrawable[NATIVE_TOKEN] = 0;
   }
 
   /// @notice Required by UUPSUpgradeable to authorize upgrades
-  function _authorizeUpgrade(
-    address newImplementation
-  ) internal override onlyOwner {}
+  function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+  modifier isPayableToken(address _payToken) {
+    require(_payToken == NATIVE_TOKEN || payableToken[_payToken], "invalid pay token");
+    _;
+  }
+
+  // MARK: USER FUNCTIONS
+
+  // function getVertion() external pure returns (string memory) {
+  //   return "v2";
+  // }
 
   /// @notice create a new order
   /// @param _orderId uuid of order
   /// @param _price price of order
   function createAndPayOrder(
     string calldata _orderId,
-    uint256 _price
-  ) external payable virtual nonReentrant {
+    uint256 _price,
+    address _payToken
+  ) external payable virtual nonReentrant isPayableToken(_payToken) {
     require(bytes(_orderId).length > 0, "Order ID cannot be empty");
     require(_price > 0, "Price must be greater than zero");
     require(orders[_orderId].buyer == address(0), "Order ID already exists");
-    require(msg.value == _price, "Incorrect payment amount");
+
+    _takeToken(_payToken, _price);
 
     Order memory newOrder = Order({
       orderId: _orderId,
       buyer: msg.sender,
       price: _price,
       status: Status.PAID,
-      createdAt: block.timestamp
+      createdAt: block.timestamp,
+      token: _payToken
     });
 
     orders[_orderId] = newOrder;
     userOrders[msg.sender].push(_orderId);
 
-    emit OrderCreated(_orderId, msg.sender, _price);
+    emit OrderPaid(_orderId, msg.sender, _price);
   }
 
   /// @notice cancel the order
   /// @param _orderId id of cancel order
-  function cancelOrder(string calldata _orderId) external nonReentrant {
-    Order storage order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
-    require(
-      msg.sender == order.buyer,
-      "Only the buyer or owner can cancel the order"
-    );
-    require(order.status == Status.PAID, "Only PAID orders can be cancelled");
-    require(
-      block.timestamp <= order.createdAt + 3 days,
-      "Can only cancel before 3 days"
-    );
+  function cancelOrder(string memory _orderId) external nonReentrant {
+    Order memory order = orders[_orderId];
+    if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+    if (msg.sender != order.buyer) revert SenderIsNotBuyer(msg.sender, order.buyer);
+    if (order.status != Status.PAID) revert OnlyPaidOrderCanBeCancel(_orderId);
 
-    uint256 refundAmount = (order.price * 99) / 100;
-    uint256 feeAmount = order.price - refundAmount;
-
-    // refund to the user
-    (bool success, ) = order.buyer.call{ value: refundAmount }("");
-    require(success, "Refund failed");
-    order.status = Status.CANCELLED;
-
-    emit OrderCancelled(_orderId, refundAmount, feeAmount);
+    _cancelOrder(_orderId);
   }
 
-  /// @notice Chuyển trạng thái đơn hàng sang Delivered
-  /// @param _orderId Mã đơn hàng cần chuyển trạng thái
-  function deliverOrder(string calldata _orderId) external onlyOwner {
-    Order storage order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
-    require(order.status == Status.PAID, "Only PAID orders can be delivered");
+  function batchCancelOrder(string[] calldata orderIds) external nonReentrant {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      string memory _orderId = orderIds[index];
+      Order memory order = orders[_orderId];
 
-    order.status = Status.DELIVERED;
-    totalWithdrawable += order.price;
+      //prettier-ignore
+      if (msg.sender != order.buyer) revert SenderIsNotBuyer(msg.sender, order.buyer);
+      if (order.status != Status.PAID) revert OnlyPaidOrderCanBeCancel(_orderId);
+      if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
 
-    emit OrderDelivered(_orderId);
+      _cancelOrder(_orderId);
+    }
   }
 
-  /// @notice Withdrawn all token
+  /// @notice admin force cancel order
+  /// @notice when order has paid and delivered still be canceled by admin
+  /// @param orderIds is a list of order
+  function forceCancelOrder(string[] calldata orderIds) external onlyOwner {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      string memory _orderId = orderIds[index];
+      Order memory order = orders[_orderId];
+
+      //prettier-ignore
+      if (order.status == Status.CANCELLED) revert OrderAlreadyCancelled(_orderId);
+      if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+
+      _cancelOrder(_orderId);
+    }
+  }
+
+  function deliverOrder(string[] memory orderIds) external onlyOwner {
+    for (uint256 index = 0; index < orderIds.length; index++) {
+      _deliverOrder(orderIds[index]);
+    }
+  }
+
   function withdrawAll() external onlyOwner nonReentrant {
-    require(totalWithdrawable > 0, "No funds available for withdrawal");
+    address owner = owner();
 
-    uint256 amount = totalWithdrawable;
-    totalWithdrawable = 0;
+    for (uint256 index = 0; index < allowedTokens.length; index++) {
+      address token = allowedTokens[index];
+      uint256 amount = withdrawable[token];
 
-    (bool success, ) = owner().call{ value: amount }("");
-    require(success, "Transfer failed.");
-
-    emit Withdrawn(amount);
+      if (amount > 0) {
+        _sendToken(token, owner, amount);
+        emit Withdrawn(token, amount);
+      }
+    }
   }
 
   /// @notice get order info
   /// @param _orderId id of the order
   /// @return Order info
-  function getOrder(
-    string calldata _orderId
-  ) external view returns (Order memory) {
+  function getOrder(string calldata _orderId) external view returns (Order memory) {
     Order memory order = orders[_orderId];
-    require(order.buyer != address(0), "Order does not exist");
     return order;
   }
 
   /// @notice get list of order of user
   /// @param _buyer address of user
   /// @return string[] list of order id
-  function getUserOrders(
-    address _buyer
-  ) external view returns (string[] memory) {
+  function getUserOrders(address _buyer) external view returns (string[] memory) {
     return userOrders[_buyer];
+  }
+
+  // MARK: ADMIN FUNCTIONS
+  function addPayableToken(address _token) external onlyOwner {
+    if (_token == address(0)) revert InvalidPaymentToken(_token);
+    require(!payableToken[_token], "already payable token");
+
+    payableToken[_token] = true;
+    allowedTokens.push(_token);
+  }
+
+  function disableToken(address _token) external onlyOwner {
+    if (_token == address(0)) revert InvalidPaymentToken(_token);
+    payableToken[_token] = false;
+  }
+
+  // MARK: INTERNAL FUNCTIONS
+  function _cancelOrder(string memory _orderId) internal {
+    Order storage order = orders[_orderId];
+    order.status = Status.CANCELLED;
+
+    // REFUND TO THE USER
+    _sendToken(order.token, order.buyer, order.price);
+
+    emit OrderCancelled(_orderId, order.buyer, order.price);
+  }
+
+  function _deliverOrder(string memory _orderId) internal {
+    Order storage order = orders[_orderId];
+    if (order.buyer == address(0)) revert OrderDoesNotExist(_orderId);
+    require(order.status == Status.PAID, "Only PAID orders can be delivered");
+
+    order.status = Status.DELIVERED;
+    withdrawable[order.token] += order.price;
+
+    emit OrderDelivered(_orderId, order.buyer);
+  }
+
+  function _sendToken(address token, address to, uint256 amount) private {
+    if (amount == 0) return;
+
+    if (token == NATIVE_TOKEN) {
+      require(address(this).balance >= amount, "Insufficient balance in contract.");
+      (bool success, ) = to.call{ value: amount }("");
+      require(success, "Transfer failed.");
+    } else {
+      IERC20Metadata(token).safeTransfer(to, amount);
+    }
+  }
+
+  function _takeToken(address token, uint256 amount) internal {
+    if (amount == 0) return;
+
+    if (token == NATIVE_TOKEN) {
+      require(msg.value >= amount, "Incorrect payment amount");
+    } else {
+      IERC20Metadata(token).safeTransferFrom(msg.sender, address(this), amount);
+    }
   }
 }

scripts/2.deploy_shopPayment.ts
@@ -8,37 +8,33 @@ async function main(step: number) {
   const network = await ethers.provider.getNetwork();
 
   if (step <= 1) {
+    console.log(`deployer ${owner.address}`);
     console.log("Deploying ShopPayment...");
-    const ShopPayment = await ethers.getContractFactory("ShopPayment");
 
     // Deploy the upgradeable contract using a UUPS proxy
-    const shopPayment = await upgrades.deployProxy(
-      ShopPayment,
-      [owner.address],
-      {
-        initializer: "initialize",
-        kind: "uups", // Specifies the UUPS proxy pattern
-      },
-    );
+    const ShopPayment = await ethers.getContractFactory("ShopPayment");
+    const shopPayment = await upgrades.deployProxy(ShopPayment, [], {
+      initializer: "initialize",
+      kind: "uups",
+    });
 
     const address = await shopPayment.getAddress();
-    console.info({ address });
-
     writeDownAddress(ContractName.ShopPayment, address, network.name);
 
     await sleep(20_000);
   }
 
-  if (step <= 2) {
-    const address = getAddress(ContractName.ShopPayment, network.name);
-    await verifyContract(address, []);
+  if (step <= 1.5) {
+    const proxy = getAddress(ContractName.ShopPayment, network.name);
+    const implement = await upgrades.erc1967.getImplementationAddress(proxy);
+    console.info("Implementation Address:", implement);
 
-    const implementation = "0x4E3DdcCeEf165fC30F876cf81b4d7a80C2A1A7bD";
-    await verifyContract(implementation, []);
+    await verifyContract(proxy, []);
+    await verifyContract(implement, []);
   }
 }
 
-main(1)
+main(1.5)
   .then(() => process.exit(0))
   .catch((error: Error) => {
     console.error(error);